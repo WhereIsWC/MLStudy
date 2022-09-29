80fed6d595361dcb90da2bb8c0dfe680c7dd3f96
WhereIsWC <649555674@qq.com>
2022-09-29 18:05:27 +0800

    added files

---
+++ MLStudy/knn/kNN.py
@@ -0,0 +1,44 @@
+from cProfile import label
+from tokenize import group
+from numpy import *
+#导入操作符模块
+import operator
+
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group,labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    #计算距离
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    #选择距离最小的k个点
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    #距离由近到远排序
+    sortedClassCount = sorted(classCount. items() ,
+    key=operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOfLines = fr.readlines()
+    numberOfLines = len(arrayOfLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOfLines:
+        line  = line.strip()
+        listFromLIne = line.split("\t")
+        returnMat[index,:] = listFromLIne[0:3]
+        classLabelVector.append(int(listFromLIne[-1]))
+        index += 1
+    return returnMat,classLabelVector 
+
